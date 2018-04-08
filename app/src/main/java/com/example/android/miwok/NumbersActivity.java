59a2590495e0fb309910b7c70882ac0fc5ee9814
TerryGlen <saispas99@gmail.com>
2018-04-08 00:34:52 -0500

    Android Basics 2:13 Added ArrayList and TextView to NumbersActivity

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,8 +3,13 @@ package com.example.android.miwok;
 import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -12,6 +17,34 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+        for( int i = 0 ; i < words.size(); i++ ){
+        Log.v("NumbersArray", "Word at index " + i +": " + words.get(i));}
+
+        //Find the rootview of the NumbersActivity
+        LinearLayout rootView = findViewById(R.id.rootView);
+
+        for (int i = 0; i < words.size() ; i++){
+            TextView wordView = new TextView(this);
+            wordView.setText(words.get(i));
+            //Add child view to Root view
+            rootView.addView(wordView);
+
+        }
+
+
     }
 
 }
