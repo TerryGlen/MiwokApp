ee34b3ce43a3e7af5c60270ccb39da4c2eb21221
TerryGlen <saispas99@gmail.com>
2018-04-09 15:04:41 -0500

    Android Basics 2:20

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -5,7 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,20 +31,18 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("eight");
         words.add("nine");
         words.add("ten");
+        ArrayAdapter<String>    itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
+        ListView listView = findViewById(R.id.list);
+        listView.setAdapter(itemsAdapter);
+
+
 
         for( int i = 0 ; i < words.size(); i++ ){
         Log.v("NumbersArray", "Word at index " + i +": " + words.get(i));}
 
         //Find the rootview of the NumbersActivity
-        LinearLayout rootView = findViewById(R.id.rootView);
 
-        for (int i = 0; i < words.size() ; i++){
-            TextView wordView = new TextView(this);
-            wordView.setText(words.get(i));
-            //Add child view to Root view
-            rootView.addView(wordView);
 
-        }
 
 
     }
